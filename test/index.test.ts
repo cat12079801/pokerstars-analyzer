1acc6250a99c4e3d6e0869c209ae0b5984119d66
cat12079801 <cat12079801@gmail.com>
2020-02-13 00:56:00 +0900

    🌟 雑にトランプクラスとそのテスト作成

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,25 @@
 /* eslint-env mocha */
 
-import * as chai from 'chai';
+import { expect } from 'chai';
 
 import PokerStarsAnalyzer from '../src/index';
+import PlayingCard from '../src/playingCard';
 
 describe('PokerStarsAnalyzerのテスト', () => {
-  it('logFilePathを取り出せる', () => {
-    const psa = new PokerStarsAnalyzer('path/to/file.log');
+  let psa: PokerStarsAnalyzer;
 
-    chai.assert.equal(psa.logFilePath, 'path/to/file.log');
+  before(() => {
+    psa = new PokerStarsAnalyzer('path/to/file.log');
+  });
+
+  it('logFilePathを取り出せる', () => {
+    expect(psa.logFilePath).to.equal('path/to/file.log');
+  });
+});
+
+describe('カード', () => {
+  it('スペードのA', () => {
+    const card = new PlayingCard('Spade', 'A');
+    expect(`${card}`).to.equal('As');
   });
 });
