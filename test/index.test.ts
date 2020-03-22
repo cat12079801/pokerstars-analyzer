67f2eda0c5ea8e9b28f4ec73394028d9e2e00176
cat12079801 <cat12079801@gmail.com>
2020-03-23 02:03:57 +0900

    🌟 トランプクラス削除

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,6 @@
 import { expect } from 'chai';
 
 import PokerStarsAnalyzer from '../src/index';
-import PlayingCard from '../src/playingCard';
 
 describe('PokerStarsAnalyzerのテスト', () => {
   let psa: PokerStarsAnalyzer;
@@ -14,12 +13,5 @@ describe('PokerStarsAnalyzerのテスト', () => {
 
   it('logFilePathを取り出せる', () => {
     expect(psa.logFilePath).to.equal('path/to/file.log');
-  });
-});
-
-describe('カード', () => {
-  it('スペードのA', () => {
-    const card = new PlayingCard('Spade', 'A');
-    expect(`${card}`).to.equal('As');
   });
 });
